8ebbe9b01998de7d8f2047e7c30d582750d1cd61
KieuGiaVi <kieugiavi1108thptlvc@gmail.com>
2024-08-06 21:59:23 +0700

    UpdatePj

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import logo from "./assets/logo.svg";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="text-red-800">Hello</h1>
+      
+    </>
+  )
+}
+
+export default App
